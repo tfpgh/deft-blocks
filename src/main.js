5216a866b9f1705452552b54dd81f43304faa93d
Toby Penner <tobypenner01@gmail.com>
2024-12-09 19:30:29 -0700

    Add analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+import { inject } from "@vercel/analytics";
 import { processSubmit } from "./generate-block";
 import "./style.css";
+
+inject();
 
 const form = document.getElementById("form");
 form.addEventListener("submit", processSubmit);
